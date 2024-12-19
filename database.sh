f8f6e2b1c14c7f36193fd49626c2bc8a8cce27f4
Ubuntu <ubuntu@ip-172-31-25-149.eu-north-1.compute.internal>
2024-12-19 10:13:49 +0000

    add database.sh and baza.txt file

---
+++ database.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+func=$1
+database=$2
+
+create_db(){
+	touch $1.txt
+}
+
+create_table(){
+	for name in ${@:2}; do
+		echo -n "$name "  >> $database.txt
+	done
+
+}
+
+insert_data(){
+	echo "" >> $database.txt 
+	for name in ${@:2}; do
+		echo -n "$name "  >> $database.txt
+	done
+}
+
+#gets index of the table, if table cannot be found returns -1
+get_index(){
+	tables=($(head -n 1 $database.txt))
+	read -r -a array <<< "$@"
+	table_chosen=$1
+	table_index=-1
+	temp_index=0
+	for name in "${tables[@]}"; do
+		if [[ $table_chosen == "${tables[$temp_index]}" ]]; then
+			table_index=$temp_index      
+		fi
+		temp_index=$(($temp_index + 1))
+ 	done		
+	return $table_index
+}
+
+select_data(){
+	while read -r linia; do
+		read -r -a tablica <<< "$linia"
+		out=()
+		for table in ${@:2}; do
+			get_index $table
+			index=$?
+			
+			if [ "$index" -le "${#tablica[@]}" ]; then
+				out+="${tablica[$index]} "
+			fi
+		done
+		echo "${out[*]}"
+	done < $database.txt
+}
+
+
+
+$func $database ${@:3}
+
